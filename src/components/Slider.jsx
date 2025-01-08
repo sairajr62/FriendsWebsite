135f7d09d07918dfefd5cb7e4a9abcff7d774b6c
sairajr62 <sairajr62@gmail.com>
2025-01-08 18:20:17 +0530

    Image Updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Autoplay, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 // Import your images
 import Image1 from "../assets/Friends1.jpg";
 import Image2 from "../assets/Friends3.jpg";
 import Image3 from "../assets/Friends6.jpg";
 import Image4 from "../assets/Friends2.jpg";
+import Image5  from "../assets/Friends16.jpg";
 
 const Slider = () => {
-  const images = [Image1, Image2, Image3, Image4];
+  const images = [Image1, Image2, Image3, Image4,Image5];
 
   return (
-    <div className="w-full max-w-3xl mx-auto p-4">
+    <div className="w-full p-4">
       <Swiper
-        modules={[Navigation, Pagination]}
         navigation
+        modules={[Navigation, Autoplay, Pagination, Scrollbar, A11y]}
         pagination={{ clickable: true }}
         spaceBetween={30}
         slidesPerView={1}
-        className="rounded-lg overflow-hidden"
+        loop={true}
+        speed={400}
+        autoplay={{
+          delay: 2000,
+        }}
+        className="w-full h-[800px] rounded-lg overflow-hidden"
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
             <img
               src={image}
-
-              className="w-full h-auto object-cover"
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
             />
           </SwiperSlide>
         ))}
